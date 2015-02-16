3d7059cd24e4b262f5f48ffc48e3302200708ead
AD-PC80MAC <naoki@alpha-do.com>
2015-02-16 12:10:04 +0900

    鍵周りのアクセサやExceptionの作成

--- src/omise/co/Omise.java
+++ src/omise/co/Omise.java
@@ -1,5 +1,7 @@
 package omise.co;
 
+import omise.co.exeption.OmiseException;
+
 public class Omise {
 	public static final String OMISE_API_VERSION = "2014-07-27";
 	public static final String OMISE_JAVA_LIB_VERSION = "1.0.0";
@@ -19,6 +21,29 @@ public class Omise {
 		}
 	}
 	
-	public static volatile String publicKey;
-	public static volatile String secretKey;
+	private static volatile String publicKey = null;
+	private static volatile String secretKey = null;
+	
+	public static void setKeys(String publicKey, String secretKey) {
+		setPublicKey(publicKey);
+		setSecretKey(secretKey);
+	}
+	
+	public static void setPublicKey(String publicKey) {
+		Omise.publicKey = publicKey;
+	}
+	public static void setSecretKey(String secretKey) {
+		Omise.secretKey = secretKey;
+	}
+	
+	public static String getPublicKey() throws OmiseException {
+		if(publicKey == null) throw new OmiseException("secret key is required.");
+		
+		return publicKey;
+	}
+	public static String getSecretKey() throws OmiseException {
+		if(secretKey == null) throw new OmiseException("secret key is required.");
+		
+		return secretKey;
+	}
 }
--- src/omise/co/exeption/OmiseException.java
+++ src/omise/co/exeption/OmiseException.java
@@ -2,5 +2,7 @@ package omise.co.exeption;
 
 @SuppressWarnings("serial")
 public class OmiseException extends Exception {
-
+	public OmiseException(String message) {
+		super(message);
+	}
 }
