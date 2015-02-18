35104ac68c174f432747598c7b9bbd50194cde30
AD-PC80MAC <naoki@alpha-do.com>
2015-02-18 11:55:58 +0900

    型が・・型が・・・

--- src/omise/co/model/OmiseList.java
+++ src/omise/co/model/OmiseList.java
@@ -1,26 +1,22 @@
 package omise.co.model;
 
-import java.util.Date;
-import java.util.List;
-
 import omise.co.net.APIResource;
 
 public abstract class OmiseList extends APIResource {
 	protected String object = null;
-	protected Date from = null;
-	protected Date to = null;
+	protected String from = null;
+	protected String to = null;
 	protected Integer offset = null;
 	protected Integer limit = null;
 	protected Integer total = null;
-	protected List<OmiseObject> data = null;
 	
 	public String getObject() {
 		return object;
 	}
-	public Date getFrom() {
+	public String getFrom() {
 		return from;
 	}
-	public Date getTo() {
+	public String getTo() {
 		return to;
 	}
 	public Integer getOffset() {
