b0d5e391c8d475b5c0f404284d80cddfaab15672
AD-PC80MAC <naoki@alpha-do.com>
2015-02-20 10:36:10 +0900

    PATCHリクエストに対応

--- src/main/java/co/omise/net/APIResource.java
+++ src/main/java/co/omise/net/APIResource.java
@@ -7,8 +7,6 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import main.java.co.omise.Omise;
@@ -18,27 +16,22 @@ import main.java.co.omise.model.OmiseError;
 import main.java.co.omise.model.OmiseObject;
 
 import org.apache.commons.codec.binary.Base64;
-import org.apache.http.Header;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicHeader;
 
 public class APIResource extends OmiseObject {
 	private static final String CHARSET = "UTF-8";
 	private static final int CONNECT_TIMEOUT = 10 * 1000;
-	private static final int REQUEST_TIMEOUT = 10 * 1000;
-	private static final int SOCKET_TIMEOUT = 10 * 1000;
-	private static final String AUTH_HOST = null;
-	private static final int AUTH_PORT = -1;
+	private static final int READ_TIMEOUT = 10 * 1000;
 	
 	protected enum RequestMethod {
-		GET, POST, PATCH, DELETE
+		GET, 
+		POST, 
+		DELETE, 
+		PATCH {
+			@Override
+			public String toString() {
+				return RequestMethod.POST.name();
+			}
+		}
 	}
 	protected enum OmiseURL {
 		API {
@@ -52,61 +45,6 @@ public class APIResource extends OmiseObject {
 			public String toString() {
 				return "https://vault.omise.co/";
 			}
-		}
-	}
-	
-	protected static APIResource requestGet(OmiseURL omiseUrl, String endPoint, Class<?> clazz) throws OmiseException, IllegalStateException, IOException {
-		HttpClient httpClient = buildHttpClient(getUseKey(omiseUrl));
-		HttpGet request = new HttpGet(omiseUrl.toString() + endPoint);
-		
-		HttpResponse response = httpClient.execute(request);
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-		
-		StringBuilder sb = new StringBuilder();
-		String buf = null;
-		while((buf = br.readLine()) != null) {
-			sb.append(buf);
-		}
-		System.out.println(sb.toString());
-		
-		return (APIResource)GSON.fromJson(sb.toString(), clazz);
-	}
-	
-	private static HttpClient buildHttpClient(String key) {
-		RequestConfig requestConfig =  RequestConfig.custom()
-				.setConnectTimeout(CONNECT_TIMEOUT)
-				.setConnectionRequestTimeout(REQUEST_TIMEOUT)
-				.setSocketTimeout(SOCKET_TIMEOUT)
-				.setCircularRedirectsAllowed(false)
-				.setRedirectsEnabled(false)
-				.setAuthenticationEnabled(true)
-				.build();
-		
-		List<Header> headers = new ArrayList<Header>();
-		headers.add(new BasicHeader("Accept-Charset", "UTF-8"));
-		headers.add(new BasicHeader("User-Agent", "OmiseJava/" + Omise.OMISE_JAVA_LIB_VERSION + " OmiseAPI/" + Omise.OMISE_API_VERSION));
-
-		BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-		credentialsProvider.setCredentials(new AuthScope(AUTH_HOST, AUTH_PORT), new UsernamePasswordCredentials(key, ""));
-		
-		HttpClient httpClient = HttpClientBuilder.create()
-				.setDefaultRequestConfig(requestConfig)
-				.setDefaultCredentialsProvider(credentialsProvider)
-				.setDefaultHeaders(headers)
-				.build();
-		
-		return httpClient;
-	}
-	
-	private static String getUseKey(OmiseURL omiseUrl) throws OmiseException {
-		switch(omiseUrl){
-			case API:
-				return Omise.getSecretKey();
-			case VAULT:
-				return Omise.getPublicKey();
-			default:
-				return null;
 		}
 	}
 	
@@ -187,8 +125,11 @@ public class APIResource extends OmiseObject {
 	 */
 	private static HttpURLConnection createConnection(OmiseURL omiseUrl, String endPoint, RequestMethod method) throws IOException, OmiseException {
 		HttpURLConnection con =  createOmiseConnection(omiseUrl, endPoint);
-		con.setRequestMethod(method.name());
+		con.setRequestMethod(method.toString());
 		con.setRequestProperty("User-Agent", "OmiseJava/" + Omise.OMISE_JAVA_LIB_VERSION + " OmiseAPI/" + Omise.OMISE_API_VERSION);
+		if(method == RequestMethod.PATCH) {
+			con.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+		}
 		
 		return con;
 	}
@@ -203,24 +144,25 @@ public class APIResource extends OmiseObject {
 	 */
 	private static HttpURLConnection createOmiseConnection(OmiseURL omiseUrl, String endPoint) throws IOException, OmiseException {
 		HttpURLConnection con = (HttpURLConnection)(new URL(omiseUrl.toString() + endPoint)).openConnection();
-		String auth = "";
-		switch (omiseUrl) {
-		case API:
-			auth = Omise.getSecretKey() + ":";
-			break;
-		case VAULT:
-			auth = Omise.getPublicKey() + ":";
-			break;
-		default:
-			break;
-		}
 		
 		con.setUseCaches(false);
 		con.setDoOutput(true);
-		con.setRequestProperty("Authorization", "Basic " + Base64.encodeBase64String(auth.getBytes(CHARSET)));
+		con.setDoInput(true);
+		con.setRequestProperty("Authorization", "Basic " + Base64.encodeBase64String(getBasicAuthString(omiseUrl).getBytes(CHARSET)));
 		con.setConnectTimeout(CONNECT_TIMEOUT);
-		//con.setReadTimeout(READ_TIMEOUT);
+		con.setReadTimeout(READ_TIMEOUT);
 		
 		return con;
+	}
+	
+	private static String getBasicAuthString(OmiseURL omiseUrl) throws OmiseException {
+		switch(omiseUrl){
+			case API:
+				return Omise.getSecretKey() + ":";
+			case VAULT:
+				return Omise.getPublicKey() + ":";
+			default:
+				return ":";
+		}
 	}
 }
