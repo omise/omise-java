7629a8d31915e7ba423f89a77abe758158688438
AD-PC80MAC <naoki@alpha-do.com>
2015-02-23 10:42:21 +0900

    Card.reload, update, destroyのさくせい

--- src/main/java/co/omise/model/Card.java
+++ src/main/java/co/omise/model/Card.java
@@ -1,10 +1,16 @@
 package main.java.co.omise.model;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 
-public class Card {
-	private static final String ENDPOINT = "card";
+import main.java.co.omise.exeption.OmiseException;
+import main.java.co.omise.net.APIResource;
+
+public class Card extends APIResource {
+	protected static final String ENDPOINT = "card";
 	
+	protected String customer_id = null;
 	protected String object = null;
 	protected String id = null;
 	protected Boolean livemode = null;
@@ -39,13 +45,13 @@ public class Card {
 	public String getCity() {
 		return city;
 	}
-	public String getPostal_code() {
+	public String getPostalCode() {
 		return postal_code;
 	}
 	public String getFinancing() {
 		return financing;
 	}
-	public String getLast_digits() {
+	public String getLastDigits() {
 		return last_digits;
 	}
 	public String getBrand() {
@@ -67,8 +73,34 @@ public class Card {
 		return created;
 	}
 	
-	public Card reload() {
-		
+	public Card reload() throws IOException, OmiseException {
+		return updateMyself((Card)request(OmiseURL.API, String.format("%s/%s/%s/%s", Customer.ENDPOINT, customer_id, ENDPOINT, id), RequestMethod.GET, null, Card.class));
+	}
+	
+	public Card update(HashMap<String, Object> params) throws IOException, OmiseException {
+		return updateMyself((Card)request(OmiseURL.API, String.format("%s/%s/%s/%s", Customer.ENDPOINT, customer_id, ENDPOINT, id), RequestMethod.PATCH, params, Card.class));
+	}
+	
+	public DeleteCard destroy() throws IOException, OmiseException {
+		return (DeleteCard)request(OmiseURL.API, String.format("%s/%s/%s/%s", Customer.ENDPOINT, customer_id, ENDPOINT, id), RequestMethod.DELETE, null, DeleteCard.class);
+	}
+	
+	private Card updateMyself(Card card) {
+		this.object = card.getObject();
+		this.id = card.getId();
+		this.livemode = card.getLivemode();
+		this.location = card.getLocation();
+		this.country = card.getCountry();
+		this.city = card.city;
+		this.postal_code = card.getPostalCode();
+		this.financing = card.getFinancing();
+		this.last_digits = card.getLastDigits();
+		this.brand = card.getBrand();
+		this.expiration_month = card.getExpiration_month();
+		this.expiration_year = card.getExpiration_year();
+		this.fingerprint = card.getFingerprint();
+		this.name = card.getName();
+		this.created = card.getCreated();
 		
 		return this;
 	}
