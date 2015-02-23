4a89e6c44cd75ed391fec1a659a98c82857c77e2
naoki <naoki@alpha-do.com>
2015-02-23 15:45:19 +0900

    Transfer.retrieve,retrieve(id),create,update,saveの作成

--- src/main/java/co/omise/model/Transfer.java
+++ src/main/java/co/omise/model/Transfer.java
@@ -1,10 +1,15 @@
 package main.java.co.omise.model;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 
+import main.java.co.omise.exeption.OmiseException;
 import main.java.co.omise.net.APIResource;
 
 public class Transfer extends APIResource {
+	protected static final String ENDPOINT = "transfers";
+	
 	protected String object = null;
 	protected String id = null;
 	protected Boolean livemode = null;
@@ -39,13 +44,16 @@ public class Transfer extends APIResource {
 	public Integer getAmount() {
 		return amount;
 	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
 	public String getCurrency() {
 		return currency;
 	}
-	public String getFailure_code() {
+	public String getFailureCode() {
 		return failure_code;
 	}
-	public String getFailure_message() {
+	public String getFailureMessage() {
 		return failure_message;
 	}
 	public String getTransaction() {
@@ -53,5 +61,45 @@ public class Transfer extends APIResource {
 	}
 	public Date getCreated() {
 		return created;
+	}
+	
+	public static Transfers retrieve() throws IOException, OmiseException {
+		return (Transfers)request(OmiseURL.API, ENDPOINT, RequestMethod.GET, null, Transfers.class);
+	}
+	
+	public static Transfer retrieve(String id) throws IOException, OmiseException {
+		return (Transfer)request(OmiseURL.API, String.format("%s/%s", ENDPOINT, id), RequestMethod.GET, null, Transfer.class);
+	}
+	
+	public static Transfer create(HashMap<String, Object> params) throws IOException, OmiseException {
+		return (Transfer)request(OmiseURL.API, ENDPOINT, RequestMethod.POST, params, Transfer.class);
+	}
+	
+	@SuppressWarnings("serial")
+	public Transfer save() throws IOException, OmiseException {
+		return update(new HashMap<String, Object>() {
+				{put("amount", amount);}
+			});
+	}
+	
+	private Transfer update(HashMap<String, Object> params) throws IOException, OmiseException {
+		return  updateMyself((Transfer)request(OmiseURL.API, String.format("%s/%s", ENDPOINT, id), RequestMethod.PATCH, params, Transfer.class));
+	}
+	
+	private Transfer updateMyself(Transfer transfer) {
+		this.object = transfer.getObject();
+		this.id = transfer.getId();
+		this.livemode = transfer.getLivemode();
+		this.location = transfer.getLocation();
+		this.sent = transfer.getSent();
+		this.paid = transfer.getPaid();
+		this.amount = transfer.getAmount();
+		this.currency = transfer.getCurrency();
+		this.failure_code = transfer.getFailureCode();
+		this.failure_message = transfer.getFailureMessage();
+		this.transaction = transfer.getTransaction();
+		this.created = transfer.getCreated();
+		
+		return this;
 	}
 }
---
+++ src/main/java/co/omise/model/Transfers.java
@@ -0,0 +1,11 @@
+package main.java.co.omise.model;
+
+import java.util.List;
+
+public class Transfers extends OmiseList {
+	protected List<Transfer> data = null;
+	
+	public List<Transfer> getData() {
+		return data;
+	}
+}
