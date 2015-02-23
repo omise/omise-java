dfad4e257f4dfad0fbef7083463c0fa5593f0f21
naoki <naoki@alpha-do.com>
2015-02-23 11:28:27 +0900

    updateの追加

--- src/main/java/co/omise/model/Charge.java
+++ src/main/java/co/omise/model/Charge.java
@@ -10,7 +10,7 @@ import main.java.co.omise.net.APIResource;
 public class Charge extends APIResource {
 	protected static final String ENDPOINT = "charges";
 
-	protected String charge = null;
+	protected String object = null;
 	protected String id = null;
 	protected String livemode = null;
 	protected String location = null;
@@ -23,16 +23,19 @@ public class Charge extends APIResource {
 	protected String transaction = null;
 	protected String failure_code = null;
 	protected String failure_message = null;
+	@Deprecated
+	protected String return_uri = null;
+	@Deprecated
+	protected String reference = null;
+	@Deprecated
+	protected String authorize_uri = null;
 	protected Card card = null;
 	protected String customer = null;
 	protected String ip = null;
 	protected Date created = null;
 	
-	public String getENDPOINT() {
-		return ENDPOINT;
-	}
-	public String getCharge() {
-		return charge;
+	public String getObject() {
+		return object;
 	}
 	public String getId() {
 		return id;
@@ -64,11 +67,23 @@ public class Charge extends APIResource {
 	public String getTransaction() {
 		return transaction;
 	}
-	public String getFailure_code() {
+	public String getFailureCode() {
 		return failure_code;
 	}
-	public String getFailure_message() {
+	public String getFailureMessage() {
 		return failure_message;
+	}
+	@Deprecated
+	public String getReturnUri() {
+		return return_uri;
+	}
+	@Deprecated
+	public String getReference() {
+		return reference;
+	}
+	@Deprecated
+	public String getAuthorizeUri() {
+		return authorize_uri;
 	}
 	public Card getCard() {
 		return card;
@@ -87,7 +102,38 @@ public class Charge extends APIResource {
 		return (Charges)request(OmiseURL.API, ENDPOINT, RequestMethod.GET, null, Charges.class);
 	}
 	
+	public static Charge retrieve(String id) throws IOException, OmiseException {
+		return (Charge)request(OmiseURL.API, String.format("%s/%s", ENDPOINT, id), RequestMethod.GET, null, Charge.class);
+	}
+	
 	public static Charge create(HashMap<String, Object> params) throws IOException, OmiseException {
 		return (Charge)request(OmiseURL.API, ENDPOINT, RequestMethod.POST, params, Charge.class);
+	}
+	
+	public Charge update(HashMap<String, Object> params) throws IOException, OmiseException {
+		return updateMyself((Charge)request(OmiseURL.API, String.format("%s/%s", ENDPOINT, id), RequestMethod.PATCH, params, Charge.class));
+	}
+	
+	private Charge updateMyself(Charge charge) {
+		this.object = charge.getObject();
+		this.id = charge.getId();
+		this.livemode = charge.getLivemode();
+		this.location = charge.getLocation();
+		this.amount = charge.getAmount();
+		this.currency = charge.getCurrency();
+		this.description = charge.getDescription();
+		this.capture = charge.getCapture();
+		this.authorized = charge.getAuthorized();
+		this.captured = charge.getCaptured();
+		this.transaction = charge.getTransaction();
+		this.return_uri = charge.getReturnUri();
+		this.reference = charge.getReference();
+		this.authorize_uri = charge.getAuthorizeUri();
+		this.card = charge.getCard();
+		this.customer = charge.getCustomer();
+		this.ip = charge.getIp();
+		this.created = charge.getCreated();
+		
+		return this;
 	}
 }
