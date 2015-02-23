d2b39b8077278c7d2fa378101dbe9d5f2eddd44b
naoki <naoki@alpha-do.com>
2015-02-23 19:02:34 +0900

    accountのテストケースの追加

---
+++ src/test/java/co/omise/AccountTest.java
@@ -0,0 +1,53 @@
+package test.java.co.omise;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import main.java.co.omise.Omise;
+import main.java.co.omise.exeption.OmiseAPIException;
+import main.java.co.omise.exeption.OmiseException;
+import main.java.co.omise.model.Account;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AccountTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Omise.setKeys("pkey", "skey");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void retrieveTest() {
+		try {
+			Account account = Account.retrieve();
+			
+			assertNotNull("リソースが取得できません", account.getObject());
+			assertEquals("取得したリソースがAccountではありません", account.getObject(), "account");
+		} catch (IOException e) {
+			fail(e.getMessage());
+		} catch (OmiseAPIException e) {
+			fail(e.getOmiseError().toString());
+		} catch (OmiseException e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
