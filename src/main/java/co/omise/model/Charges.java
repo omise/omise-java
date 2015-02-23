14add7d4f83e89340df23601721e50ad37b62a9d
naoki <naoki@alpha-do.com>
2015-02-23 11:04:27 +0900

    List系のCharges追加

---
+++ src/main/java/co/omise/model/Charges.java
@@ -0,0 +1,11 @@
+package main.java.co.omise.model;
+
+import java.util.List;
+
+public class Charges extends OmiseList {
+	protected List<Charge> data = null;
+	
+	public List<Charge> getData() {
+		return data;
+	}
+}
