8728049f19740af711e2507b34af40d2c29ebbc7
naoki <naoki@alpha-do.com>
2015-02-23 15:57:52 +0900

    retrieve, retrieve(id)の作成

--- src/main/java/co/omise/model/Transaction.java
+++ src/main/java/co/omise/model/Transaction.java
@@ -1,10 +1,14 @@
 package main.java.co.omise.model;
 
+import java.io.IOException;
 import java.util.Date;
 
+import main.java.co.omise.exeption.OmiseException;
 import main.java.co.omise.net.APIResource;
 
 public class Transaction extends APIResource {
+	protected static final String ENDPOINT = "transactions";
+	
 	protected String object = null;
 	protected String id = null;
 	protected String type = null;
@@ -29,5 +33,13 @@ public class Transaction extends APIResource {
 	}
 	public Date getCreated() {
 		return created;
+	}
+	
+	public static Transactions retrieve() throws IOException, OmiseException {
+		return (Transactions)request(OmiseURL.API, ENDPOINT, RequestMethod.GET, null, Transactions.class);
+	}
+	
+	public static Transaction retrieve(String id) throws IOException, OmiseException {
+		return (Transaction)request(OmiseURL.API, String.format("%s/%s", ENDPOINT, id), RequestMethod.GET, null, Transaction.class);
 	}
 }
