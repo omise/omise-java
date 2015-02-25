df59af1280ab0566a9ff82264d0af58501b3ebf0
naoki <naoki@alpha-do.com>
2015-02-25 15:26:51 +0900

    テスト用の鍵アップ

--- src/test/java/co/omise/OmiseSetting.java
+++ src/test/java/co/omise/OmiseSetting.java
@@ -4,6 +4,6 @@ import main.java.co.omise.Omise;
 
 public class OmiseSetting {
 	public static void setKeys() {
-		Omise.setKeys("pkey", "skey");
+		Omise.setKeys("pkey_test_4z69iiktjyzq55zeqv6", "skey_test_4z69ilgpblls3m2y4gi");
 	}
 }
