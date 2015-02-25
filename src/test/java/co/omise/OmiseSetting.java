b9f1a257c3b310b6d4bd1dbe1b2db685067a3382
naoki <naoki@alpha-do.com>
2015-02-25 15:41:03 +0900

    どや

--- src/test/java/co/omise/OmiseSetting.java
+++ src/test/java/co/omise/OmiseSetting.java
@@ -4,6 +4,7 @@ import main.java.co.omise.Omise;
 
 public class OmiseSetting {
 	public static void setKeys() {
-		Omise.setKeys("pkey_test_4z69iiktjyzq55zeqv6", "skey_test_4z69ilgpblls3m2y4gi");
+		//Omise.setKeys("pkey_test_4z69iiktjyzq55zeqv6", "skey_test_4z69ilgpblls3m2y4gi");
+		Omise.setKeys("pkey_test_4y9cewl0s1osh44ouud", "skey_test_4y9cewl0rgwji2kbbcb");
 	}
 }
