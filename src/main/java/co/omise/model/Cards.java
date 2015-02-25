76e0227d11b029970a83b49edf69a304b66a2474
naoki <naoki@alpha-do.com>
2015-02-25 11:42:43 +0900

    retrieve(id)の実装

--- src/main/java/co/omise/model/Cards.java
+++ src/main/java/co/omise/model/Cards.java
@@ -1,11 +1,28 @@
 package main.java.co.omise.model;
 
+import java.io.IOException;
 import java.util.List;
 
+import main.java.co.omise.exeption.OmiseException;
+
 public class Cards extends OmiseList {
+	protected String customer_id = null;
 	protected List<Card> data = null;
 	
 	public List<Card> getData() {
 		return data;
+	}
+	
+	protected Cards setCustomerID(String id) {
+		customer_id = id;
+		for(Card card : data) {
+			card.customer_id = id;
+		}
+		
+		return this;
+	}
+	
+	public Card retrieve(String id) throws IOException, OmiseException {
+		return (Card)request(OmiseURL.API, String.format("%s/%s/%s/%s", Customer.ENDPOINT, customer_id, Card.ENDPOINT, id), RequestMethod.GET, null, Card.class);
 	}
 }
