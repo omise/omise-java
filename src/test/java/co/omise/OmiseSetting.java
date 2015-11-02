6e572d849a35f57ba309e60a3787d6910061f7a2
Tin Hanchanachai <hanc.tin@gmail.com>
2015-11-02 14:27:21 +0700

    Fixed Circleci fail (Coz api key )

--- src/test/java/co/omise/OmiseSetting.java
+++ src/test/java/co/omise/OmiseSetting.java
@@ -3,6 +3,6 @@ package co.omise;
 
 public class OmiseSetting {
 	public static void setKeys() {
-		Omise.setKeys("pkey_test_51w0nbz4jncdc32hp9y", "skey_test_51w0nbz4h1ajmlumo5m");
+		Omise.setKeys("pkey_test_515sxqadnwjpwawmus6", "skey_test_515sxqkqhn3ao9b93lz");
 	}
 }
