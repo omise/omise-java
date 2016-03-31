4851f2468d86c2e7dda244977a7992a32a8efa21
Chakrit Wichian <service@chakrit.net>
2016-03-31 18:08:57 +0700

    Adds a couple of live tests.

--- src/test/java/co/omise/ClientTest.java
+++ src/test/java/co/omise/ClientTest.java
@@ -1,6 +1,11 @@
 package co.omise;
 
+import co.omise.models.Balance;
+import co.omise.models.OmiseError;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class ClientTest extends OmiseTest {
     @Test
@@ -10,5 +15,23 @@ public class ClientTest extends OmiseTest {
             fail("exception expected");
         } catch (NullPointerException e) {
         }
+    }
+
+    @Test
+    @Ignore("only hit the network when we need to.")
+    public void testLiveError() throws IOException {
+        try {
+            new Client("skey_test_123").account().get();
+        } catch (OmiseError e) {
+            assertEquals("authentication_failure", e.getCode());
+        }
+    }
+
+    @Test
+    @Ignore("adds your skey here to do a live test.")
+    public void testLiveFetch() throws IOException {
+        Client client = new Client("skey_test_123");
+        Balance balance = client.balance().get();
+        assertEquals(16741576, balance.getTotal());
     }
 }
