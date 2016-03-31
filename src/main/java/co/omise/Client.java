46f3bec8e3dc60bba83c4ae9f0b6435e83586a12
Chakrit Wichian <service@chakrit.net>
2016-03-31 18:09:29 +0700

    Pin *.omise.co certificates.

--- src/main/java/co/omise/Client.java
+++ src/main/java/co/omise/Client.java
@@ -2,7 +2,10 @@ package co.omise;
 
 import co.omise.resources.*;
 import com.google.common.base.Preconditions;
+import okhttp3.CertificatePinner;
 import okhttp3.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
 
 public class Client {
     private final Config config;
@@ -31,9 +34,7 @@ public class Client {
         Preconditions.checkNotNull(secretKey);
 
         config = new Config(apiVersion, publicKey, secretKey);
-        httpClient = new OkHttpClient.Builder()
-                .addInterceptor(new Configurer(config))
-                .build();
+        httpClient = buildHttpClient(config);
 
         account = new AccountResource(httpClient);
         balance = new BalanceResource(httpClient);
@@ -45,6 +46,18 @@ public class Client {
         tokens = new TokenResource(httpClient);
         transactions = new TransactionResource(httpClient);
         transfers = new TransferResource(httpClient);
+    }
+
+    protected OkHttpClient buildHttpClient(Config config) {
+        return new OkHttpClient.Builder()
+                .addInterceptor(new Configurer(config))
+                .certificatePinner(new CertificatePinner.Builder()
+                        .add("api.omise.co", "sha256/maqNsxEnwszR+xCmoGUiV636PvSM5zvBIBuupBn9AB8=")
+                        .add("api.omise.co", "sha256/XT9zbxeAYOl+K6JJ3CkAN42+HiQf9vdIY57+yn3mTNk=")
+                        .add("vault.omise.co", "sha256/maqNsxEnwszR+xCmoGUiV636PvSM5zvBIBuupBn9AB8=")
+                        .add("vault.omise.co", "sha256/XT9zbxeAYOl+K6JJ3CkAN42+HiQf9vdIY57+yn3mTNk=")
+                        .build())
+                .build();
     }
 
     public AccountResource account() {
