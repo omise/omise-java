8c6c257c482c3c04d18505ccda0be932e6df812f
Nami <namnamsalimi@gmail.com>
2018-06-27 11:46:58 +0700

    Changed ChargeRequestTest to use test Requester from parent class

--- src/test/java/co/omise/requests/ChargeRequestTest.java
+++ src/test/java/co/omise/requests/ChargeRequestTest.java
@@ -1,13 +1,10 @@
 package co.omise.requests;
 
-import co.omise.Serializer;
 import co.omise.models.Charge;
 import co.omise.models.OmiseException;
 import org.junit.Test;
 
 import java.io.IOException;
-
-import static junit.framework.TestCase.assertEquals;
 
 public class ChargeRequestTest extends RequestTest {
     private final String CHARGE_ID = "chrg_test_4yq7duw15p9hdrjp8oq";
@@ -15,7 +12,7 @@ public class ChargeRequestTest extends RequestTest {
     @Test
     public void testGet() throws IOException, OmiseException {
         //TODO change this to getTestRequester once the Balance PR is merged
-        Requester requester = new RequesterImpl(testClient(), Serializer.defaultSerializer());
+        Requester requester = getTestRequester();
         Request<Charge> getChargeRequest = new Charge.GetRequestBuilder(CHARGE_ID).build();
         Charge charge = requester.sendRequest(getChargeRequest, Charge.class);
 
