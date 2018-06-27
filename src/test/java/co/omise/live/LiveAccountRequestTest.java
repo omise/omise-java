089b2fb490edd295f656f470188b7007da73d302
Nami <namnamsalimi@gmail.com>
2018-06-27 10:10:35 +0700

    Removed unnecessary comments from LiveAccountRequestTest

--- src/test/java/co/omise/live/LiveAccountRequestTest.java
+++ src/test/java/co/omise/live/LiveAccountRequestTest.java
@@ -7,21 +7,18 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class LiveAccountRequestTest extends BaseLiveTest{
+public class LiveAccountRequestTest extends BaseLiveTest {
 
     @Test
     @Ignore("only hit when test on live.")
     public void getAccount_success() throws Exception {
-        // Give
         Client client = new Client(getPublicKey(), getSecretKey());
 
-        // When
         Request<Account> getAccountRequest = new Account.GetRequestBuilder().build();
         Account actualAccount = client.sendRequest(getAccountRequest, Account.class);
 
         System.out.println("Account retrieved: " + actualAccount.getEmail());
 
-        // Then
         Assert.assertEquals(getUserEmail(), actualAccount.getEmail());
     }
 }
