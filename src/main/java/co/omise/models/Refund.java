f8a11c6cc1bd7546a0146a08c93b6c3661c905b6
Natthawut Hemathulin <natthawut1991@gmail.com>
2018-07-20 10:17:00 +0700

    Change refund request flow (#36)
    * Changed flow of create refund.
    
    * Changed flow of retrieve refund.
    
    * Changed flow of retrieve refund list.
    
    * Added refund doc.
    
    * Moved LIVETEST_CHARGE and LIVETEST_REFUND to LiveRefundRequestTest.
    
    * Updated Java version in omise-java_test.iml.
    
    * Added put metadata with key/value.

--- src/main/java/co/omise/models/Refund.java
+++ src/main/java/co/omise/models/Refund.java
@@ -1,7 +1,14 @@
 package co.omise.models;
 
+import co.omise.Endpoint;
+import co.omise.requests.RequestBuilder;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Maps;
+import okhttp3.HttpUrl;
+import okhttp3.RequestBody;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -56,19 +63,96 @@ public class Refund extends Model {
         this.metadata = metadata;
     }
 
-    public static class Create extends Params {
+    /**
+     * The {@link RequestBuilder} class for creating a Refund.
+     */
+    public static class CreateRequestBuilder extends RequestBuilder<Refund> {
+        private String chargeId;
         @JsonProperty
         private long amount;
         @JsonProperty
         private Map<String, Object> metadata;
 
-        public Create amount(long amount) {
+        public CreateRequestBuilder(String chargeId) {
+            this.chargeId = chargeId;
+        }
+
+        @Override
+        protected HttpUrl path() {
+            return buildUrl(Endpoint.API, "charges", chargeId, "refunds");
+        }
+
+        @Override
+        protected String method() {
+            return POST;
+        }
+
+        @Override
+        protected RequestBody payload() throws IOException {
+            return serialize();
+        }
+
+        public CreateRequestBuilder amount(long amount) {
             this.amount = amount;
             return this;
         }
 
-        public Create metadata(Map<String, Object> metadata) {
+        public CreateRequestBuilder metadata(Map<String, Object> metadata) {
             this.metadata = metadata;
+            return this;
+        }
+
+        public CreateRequestBuilder metadata(String key, Object value) {
+            HashMap<String, Object> tempMap = Maps.newHashMap();
+            if (metadata != null) {
+                tempMap.putAll(metadata);
+            }
+            tempMap.put(key, value);
+            this.metadata = new HashMap<>(tempMap);
+            return this;
+        }
+    }
+
+    /**
+     * The {@link RequestBuilder} class for retrieving a particular Refund.
+     */
+    public static class GetRequestBuilder extends RequestBuilder<Refund> {
+        private final String chargeId;
+        private final String refundId;
+
+        public GetRequestBuilder(String chargeId, String refundId) {
+            this.chargeId = chargeId;
+            this.refundId = refundId;
+        }
+
+        @Override
+        protected HttpUrl path() {
+            return buildUrl(Endpoint.API, "charges", chargeId, "refunds", refundId);
+        }
+    }
+
+    /**
+     * The {@link RequestBuilder} class for retrieving all Refunds that belong to a particular Charge.
+     */
+    public static class ListRequestBuilder extends RequestBuilder<ScopedList<Refund>> {
+        private final String chargeId;
+        private ScopedList.Options options;
+
+        public ListRequestBuilder(String chargeId) {
+            this.chargeId = chargeId;
+        }
+
+        @Override
+        protected HttpUrl path() {
+            if (options == null) {
+                options = new ScopedList.Options();
+            }
+
+            return buildUrl(Endpoint.API, "charges", chargeId, "refunds");
+        }
+
+        public ListRequestBuilder options(ScopedList.Options options) {
+            this.options = options;
             return this;
         }
     }
---
+++ src/test/java/co/omise/live/LiveRefundRequestTest.java
@@ -0,0 +1,69 @@
+package co.omise.live;
+
+import co.omise.Client;
+import co.omise.models.OmiseException;
+import co.omise.models.Refund;
+import co.omise.models.ScopedList;
+import co.omise.requests.Request;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class LiveRefundRequestTest extends BaseLiveTest {
+
+    private final String LIVETEST_CHARGE = "[YOUR_CHARGE]";
+    private final String LIVETEST_REFUND = "[YOUR_REFUND]";
+
+    private Client client;
+
+    @Before
+    public void setup() throws Exception {
+        client = getLiveClient();
+    }
+
+    @Test
+    @Ignore("only hit the network when we need to.")
+    public void testLiveCreateRefund() throws IOException, OmiseException {
+        Request<Refund> request = new Refund.CreateRequestBuilder(LIVETEST_CHARGE)
+                .amount(10000)
+                .metadata("description", "DESCRIPTION")
+                .metadata("invoice_id", "inv_N1ayTWJ2FV")
+                .build();
+        Refund refund = client.sendRequest(request, Refund.class);
+
+        System.out.println("created refund: " + refund.getId());
+
+        assertEquals("DESCRIPTION", refund.getMetadata().get("description"));
+        assertEquals("inv_N1ayTWJ2FV", refund.getMetadata().get("invoice_id"));
+    }
+
+    @Test
+    @Ignore("only hit the network when we need to.")
+    public void testLiveGetRefund() throws IOException, OmiseException {
+        Request<Refund> request = new Refund.GetRequestBuilder(LIVETEST_CHARGE, LIVETEST_REFUND).build();
+        Refund refund = client.sendRequest(request, Refund.class);
+
+        System.out.println("retrieved refund: " + refund.getId());
+
+        assertEquals(LIVETEST_REFUND, refund.getId());
+    }
+
+    @Test
+    @Ignore("only hit the network when we need to.")
+    public void testLiveGetRefundList() throws IOException, OmiseException {
+        Request<ScopedList<Refund>> request = new Refund.ListRequestBuilder(LIVETEST_CHARGE).build();
+        ScopedList<Refund> refunds = client.sendRequest(request, new TypeReference<ScopedList<Refund>>() {});
+
+        System.out.println("retrieved refund list total no.: " + refunds.getTotal());
+
+        assertNotNull(refunds.getData().get(0));
+    }
+}
