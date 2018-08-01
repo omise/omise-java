8d409b154e69886ad1896e3e7bc7665844c55ebc
Nami <namnamsalimi@gmail.com>
2018-08-01 19:25:16 +0700

    Added local tests for get link list request

--- src/test/java/co/omise/requests/LinkRequestTest.java
+++ src/test/java/co/omise/requests/LinkRequestTest.java
@@ -2,6 +2,8 @@ package co.omise.requests;
 
 import co.omise.models.Link;
 import co.omise.models.OmiseException;
+import co.omise.models.ScopedList;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -38,5 +40,21 @@ public class LinkRequestTest extends RequestTest {
         assertRequested("GET", "/links/" + LINK_ID, 200);
         assertEquals(LINK_ID, link.getId());
         assertTrue(link.isMultiple());
+    }
+
+    @Test
+    public void testList() throws IOException, OmiseException {
+        Request<ScopedList<Link>> request =
+                new Link.ListRequestBuilder().build();
+        ScopedList<Link> list = getTestRequester().sendRequest(request, new TypeReference<ScopedList<Link>>() {
+        });
+
+        assertRequested("GET", "/links", 200);
+        assertEquals(2, list.getTotal());
+        assertEquals(10, list.getLimit());
+
+        Link link = list.getData().get(0);
+        assertEquals("link", link.getObject());
+        assertEquals(LINK_ID, link.getId());
     }
 }
