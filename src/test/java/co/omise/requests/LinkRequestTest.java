6b6fedfa272ebf26b9bd207ebb741f96784fbb19
Nami <namnamsalimi@gmail.com>
2018-08-01 17:04:03 +0700

    Added local tests for create link request

---
+++ src/test/java/co/omise/requests/LinkRequestTest.java
@@ -0,0 +1,29 @@
+package co.omise.requests;
+
+import co.omise.models.Link;
+import co.omise.models.OmiseException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class LinkRequestTest extends RequestTest {
+    private static final String LINK_ID = "link_test_5csdepgdzorb7de56ff";
+
+    @Test
+    public void testCreate() throws IOException, OmiseException {
+        Request<Link> request = new Link.CreateRequestBuilder()
+                .amount(100000)
+                .currency("thb")
+                .title("Omise Sale")
+                .description("Medium size T-Shirt (Blue)")
+                .multiple(true)
+                .build();
+
+        Link link = getTestRequester().sendRequest(request, Link.class);
+
+        assertRequested("POST", "/links", 200);
+        assertEquals(LINK_ID, link.getId());
+        assertFalse(link.isLiveMode());
+        assertTrue(link.isMultiple());
+    }
+}
