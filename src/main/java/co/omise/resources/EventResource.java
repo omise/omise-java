c820eca2bc1fcd1f5d0508fbe1203092249cd3db
Nami <namnamsalimi@gmail.com>
2018-08-02 16:42:38 +0700

    Removed EventResources and its usage in Client

--- src/main/java/co/omise/resources/EventResource.java
+++
@@ -1,9 +0,0 @@
-package co.omise.resources;
-
-import okhttp3.OkHttpClient;
-
-public class EventResource extends Resource {
-    public EventResource(OkHttpClient httpClient) {
-        super(httpClient);
-    }
-}
