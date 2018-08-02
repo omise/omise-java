d3f38286dfc7e62945f243ce6623775e77fab9f7
Nami <namnamsalimi@gmail.com>
2018-08-02 16:39:04 +0700

    Added live tests for get event list request

--- src/test/java/co/omise/live/LiveEventRequestTest.java
+++ src/test/java/co/omise/live/LiveEventRequestTest.java
@@ -1,12 +1,16 @@
 package co.omise.live;
 
 import co.omise.Client;
-import co.omise.models.Charge;
-import co.omise.models.Event;
+import co.omise.models.*;
 import co.omise.requests.Request;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.*;
 
 public class LiveEventRequestTest extends BaseLiveTest {
@@ -27,5 +31,48 @@ public class LiveEventRequestTest extends BaseLiveTest {
         // These can break depending on the EVENT_ID that's been picked
         assertEquals(EVENT_ID, event.getId());
         assertTrue(event.getData() instanceof Charge);
+    }
+
+    @Test
+    @Ignore("only hit the network when we need to.")
+    public void testLiveEventListGet() throws Exception {
+        Client client = getLiveClient();
+
+        Request<ScopedList<Event>> request = new Event.ListRequestBuilder().build();
+
+
+        ScopedList<Event> events = client.sendRequest(request, new TypeReference<ScopedList<Event>>() {
+        });
+
+        assertEquals(20, events.getLimit());
+        assertEquals(114, events.getTotal()); // This can easily break as you add events,
+
+        Event event = events.getData().get(0);
+        assertNotNull(event);
+    }
+
+    @Test
+    @Ignore("only hit the network when we need to.")
+    public void testLiveEventListGet_withOptions() throws Exception {
+        Client client = getLiveClient();
+
+        Request<ScopedList<Event>> request =
+                new Event.ListRequestBuilder()
+                        .options(new ScopedList.Options()
+                                .limit(5)
+                                .order(Ordering.ReverseChronological))
+                        .build();
+
+
+        ScopedList<Event> events = client.sendRequest(request, new TypeReference<ScopedList<Event>>() {
+        });
+
+        assertEquals(5, events.getLimit());
+        assertEquals(5, events.getData().size());
+        assertEquals(Ordering.ReverseChronological, events.getOrder());
+        assertEquals(114, events.getTotal()); // This can easily break as you add events,
+
+        Event event = events.getData().get(0);
+        assertNotNull(event);
     }
 }
