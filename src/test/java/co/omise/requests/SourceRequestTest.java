eb74dca5c9e1a77d8edd10c7d2572f9cc38903f6
Nami <namnamsalimi@gmail.com>
2018-08-02 13:36:21 +0700

    Changed local tests for create source request

--- src/test/java/co/omise/requests/SourceRequestTest.java
+++ src/test/java/co/omise/requests/SourceRequestTest.java
@@ -1,30 +1,28 @@
-package co.omise.resources;
+package co.omise.requests;
 
 import co.omise.models.OmiseException;
 import co.omise.models.Source;
-
 import co.omise.models.SourceType;
 import org.junit.Test;
 
 import java.io.IOException;
 
-public class SourceResourceTest extends ResourceTest {
+public class SourceRequestTest extends RequestTest {
 
     @Test
     public void testCreate() throws IOException, OmiseException {
-        Source source = resource().create(new Source.Create()
+        Request<Source> request = new Source.CreateRequestBuilder()
                 .amount(100000)
                 .currency("thb")
-                .type(SourceType.Alipay));
+                .type(SourceType.Alipay)
+                .build();
+
+        Source source = getTestRequester().sendRequest(request, Source.class);
 
         assertRequested("POST", "/sources", 200);
         assertEquals(100000L, source.getAmount());
         assertEquals("thb", source.getCurrency());
         assertEquals("redirect", source.getFlow().toString());
         assertEquals("alipay", source.getType().toString());
-    }
-
-    private SourceResource resource() {
-        return new SourceResource(testClient());
     }
 }
