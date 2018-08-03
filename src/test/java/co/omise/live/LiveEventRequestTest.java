543f49462302910449e89cbf4ae16f72ff8aec98
Nami <namnamsalimi@gmail.com>
2018-08-03 11:43:05 +0700

    Fixed some code correctness issues in LiveEventRequestTest

--- src/test/java/co/omise/live/LiveEventRequestTest.java
+++ src/test/java/co/omise/live/LiveEventRequestTest.java
@@ -1,27 +1,33 @@
 package co.omise.live;
 
 import co.omise.Client;
-import co.omise.models.*;
+import co.omise.models.Charge;
+import co.omise.models.Event;
+import co.omise.models.Ordering;
+import co.omise.models.ScopedList;
 import co.omise.requests.Request;
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.IOException;
-
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class LiveEventRequestTest extends BaseLiveTest {
-
     private static final String EVENT_ID = "YOUR_EVENT";
+
+    private Client client;
+
+    @Before
+    public void setup() throws Exception {
+        client = getLiveClient();
+    }
 
     @Test
     @Ignore("only hit when test on live.")
     public void testGetEventSuccess() throws Exception {
-        Client client = getLiveClient();
-
         Request<Event> request = new Event.GetRequestBuilder(EVENT_ID).build();
         Event event = client.sendRequest(request, Event.class);
 
@@ -36,10 +42,7 @@ public class LiveEventRequestTest extends BaseLiveTest {
     @Test
     @Ignore("only hit the network when we need to.")
     public void testLiveEventListGet() throws Exception {
-        Client client = getLiveClient();
-
         Request<ScopedList<Event>> request = new Event.ListRequestBuilder().build();
-
 
         ScopedList<Event> events = client.sendRequest(request, new TypeReference<ScopedList<Event>>() {
         });
@@ -54,8 +57,6 @@ public class LiveEventRequestTest extends BaseLiveTest {
     @Test
     @Ignore("only hit the network when we need to.")
     public void testLiveEventListGet_withOptions() throws Exception {
-        Client client = getLiveClient();
-
         Request<ScopedList<Event>> request =
                 new Event.ListRequestBuilder()
                         .options(new ScopedList.Options()
@@ -63,14 +64,13 @@ public class LiveEventRequestTest extends BaseLiveTest {
                                 .order(Ordering.ReverseChronological))
                         .build();
 
-
         ScopedList<Event> events = client.sendRequest(request, new TypeReference<ScopedList<Event>>() {
         });
 
         assertEquals(5, events.getLimit());
         assertEquals(5, events.getData().size());
         assertEquals(Ordering.ReverseChronological, events.getOrder());
-        assertEquals(114, events.getTotal()); // This can easily break as you add events,
+        assertTrue(events.getTotal() > 0);
 
         Event event = events.getData().get(0);
         assertNotNull(event);
