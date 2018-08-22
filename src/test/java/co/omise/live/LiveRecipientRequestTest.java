97203344260658efcda324a4ef290454d94e483f
Nami <namnamsalimi@gmail.com>
2018-08-22 12:38:09 +0700

    Added live test for UpdateRecipient request

--- src/test/java/co/omise/live/LiveRecipientRequestTest.java
+++ src/test/java/co/omise/live/LiveRecipientRequestTest.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LiveRecipientRequestTest extends BaseLiveTest {
     private Client client;
@@ -71,5 +73,43 @@ public class LiveRecipientRequestTest extends BaseLiveTest {
         assertEquals(createdRecipient.getId(), retrievedRecipient.getId());
         assertEquals(createdRecipient.getName(), retrievedRecipient.getName());
         assertEquals(createdRecipient.getBankAccount().getBrand(), retrievedRecipient.getBankAccount().getBrand());
+    }
+
+    @Test
+    @Ignore("only hit the network when we need to.")
+    public void testLiveUpdateRecipient() throws IOException, OmiseException {
+        Request<Recipient> createRequest = new Recipient.CreateRequestBuilder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .description("Default recipient")
+                .type(RecipientType.Individual)
+                .bankAccount(new BankAccount.Params()
+                        .brand("kbank")
+                        .number("1234567890")
+                        .name("SOMCHAI PRASERT"))
+                .build();
+
+        Recipient createdRecipient = client.sendRequest(createRequest);
+
+        System.out.println("created recipient: " + createdRecipient.getId());
+
+        Request<Recipient> updateRequest = new Recipient.UpdateRequestBuilder(createdRecipient.getId())
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .description("Default recipient")
+                .type(RecipientType.Individual)
+                .bankAccount(new BankAccount.Params()
+                        .brand("kbank")
+                        .number("1234567890")
+                        .name("John Doe"))
+                .build();
+
+        Recipient updatedRecipient = client.sendRequest(updateRequest);
+
+        System.out.println("updated recipient: " + updatedRecipient.getId());
+
+        assertEquals(createdRecipient.getId(), updatedRecipient.getId());
+        assertNotEquals(createdRecipient.getBankAccount().getName(), updatedRecipient.getBankAccount().getName());
+        assertEquals("John Doe", updatedRecipient.getBankAccount().getName());
     }
 }
