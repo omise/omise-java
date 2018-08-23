aa1665a6b4e7bb4e846f38114e50d45f5ca64739
Nami <namnamsalimi@gmail.com>
2018-08-23 12:41:19 +0700

    Removed ReceiptResource completely

--- src/main/java/co/omise/resources/ReceiptResource.java
+++
@@ -1,9 +0,0 @@
-package co.omise.resources;
-
-import okhttp3.OkHttpClient;
-
-public class ReceiptResource extends Resource {
-    public ReceiptResource(OkHttpClient httpClient) {
-        super(httpClient);
-    }
-}
