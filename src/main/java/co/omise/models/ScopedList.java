a97a8ccf9e14725d9ba29b62b6e5f712d67e4c6f
Nami <namnamsalimi@gmail.com>
2018-08-24 17:05:06 +0700

    Replaced Guava ImmutableMap with native immutable Map in ScopedList

--- src/main/java/co/omise/models/ScopedList.java
+++ src/main/java/co/omise/models/ScopedList.java
@@ -1,10 +1,13 @@
 package co.omise.models;
 
 import co.omise.Serializer;
-import com.google.common.collect.ImmutableMap;
 import okhttp3.RequestBody;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ScopedList<T extends Model> extends OmiseList<T> {
     private DateTime from;
@@ -77,32 +80,31 @@ public class ScopedList<T extends Model> extends OmiseList<T> {
         }
 
         @Override
-        public ImmutableMap<String, String> query(Serializer serializer) {
+        public Map<String, String> query(Serializer serializer) {
             if (serializer == null) {
                 serializer = Serializer.defaultSerializer();
             }
 
-            ImmutableMap.Builder<String, String> builder = new ImmutableMap.Builder<>();
+            Map<String, String> map = new HashMap<>();
             DateTimeFormatter formatter = serializer.dateTimeFormatter();
 
             if (offset != null) {
-                builder = builder.put("offset", offset.toString());
+                map.put("offset", offset.toString());
             }
             if (limit != null) {
-                builder = builder.put("limit", limit.toString());
+                map.put("limit", limit.toString());
             }
             if (from != null) {
-                builder = builder.put("from", formatter.print(from));
+                map.put("from", formatter.print(from));
             }
             if (to != null) {
-                builder = builder.put("to", formatter.print(to));
+                map.put("to", formatter.print(to));
             }
 
             if (order != null) {
-                builder = builder.put("order", serializer.serializeToQueryParams(order));
+                map.put("order", serializer.serializeToQueryParams(order));
             }
-
-            return builder.build();
+            return Collections.unmodifiableMap(map);
         }
 
         @Override
