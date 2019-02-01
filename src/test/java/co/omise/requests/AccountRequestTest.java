3c645392bd6ffbfd8448f70a1c3256fc63706552
Nami <namnamsalimi@gmail.com>
2019-02-01 13:50:15 +0700

    Currency field  in account model (#66)
    * Added currency field to the Account model, along with local and live tests
    
    * Cleaned up both local and live tests for Account request

--- src/test/java/co/omise/requests/AccountRequestTest.java
+++ src/test/java/co/omise/requests/AccountRequestTest.java
@@ -7,15 +7,18 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class AccountRequestTest extends RequestTest {
+
     @Test
     public void testGet() throws IOException, OmiseException {
         Requester requester = getTestRequester();
         Request<Account> getAccountRequest = new Account.GetRequestBuilder().build();
         Account account = requester.sendRequest(getAccountRequest);
+
         assertRequested("GET", "/account", 200);
 
         assertEquals("chakrit@omise.co", account.getEmail());
         assertEquals("acct_4yq6tcsyoged5c0ocxd", account.getId());
+        assertEquals("thb", account.getCurrency());
         assertEquals("account", account.getObject());
         assertEquals("/account", account.getLocation());
     }
