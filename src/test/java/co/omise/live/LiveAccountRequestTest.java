8e63ee7c234972e05866aeb35930ddfd9789960a
Nami <namnamsalimi@gmail.com>
2019-02-01 09:52:45 +0700

    Added currency field to the Account model, along with local and live tests

--- src/test/java/co/omise/live/LiveAccountRequestTest.java
+++ src/test/java/co/omise/live/LiveAccountRequestTest.java
@@ -3,15 +3,16 @@ package co.omise.live;
 import co.omise.Client;
 import co.omise.models.Account;
 import co.omise.requests.Request;
-import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LiveAccountRequestTest extends BaseLiveTest {
 
     @Test
     @Ignore("only hit when test on live.")
-    public void getAccount_success() throws Exception {
+    public void getAccount_EmailSuccess() throws Exception {
         Client client = getLiveClient();
 
         Request<Account> getAccountRequest = new Account.GetRequestBuilder().build();
@@ -19,6 +20,19 @@ public class LiveAccountRequestTest extends BaseLiveTest {
 
         System.out.println("Account retrieved: " + actualAccount.getEmail());
 
-        Assert.assertEquals(getUserEmail(), actualAccount.getEmail());
+        assertEquals(getUserEmail(), actualAccount.getEmail());
+    }
+
+    @Test
+    @Ignore("only hit when test on live.")
+    public void getAccount_CurrencySuccess() throws Exception {
+        Client client = getLiveClient();
+
+        Request<Account> request = new Account.GetRequestBuilder().build();
+        Account account = client.sendRequest(request);
+
+        System.out.println("Account retrieved: " + account.getEmail());
+
+        assertEquals("thb", account.getCurrency());
     }
 }
--- src/test/java/co/omise/requests/AccountRequestTest.java
+++ src/test/java/co/omise/requests/AccountRequestTest.java
@@ -7,16 +7,48 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class AccountRequestTest extends RequestTest {
+
     @Test
     public void testGet() throws IOException, OmiseException {
         Requester requester = getTestRequester();
         Request<Account> getAccountRequest = new Account.GetRequestBuilder().build();
         Account account = requester.sendRequest(getAccountRequest);
+
+        assertRequested("GET", "/account", 200);
+
+        assertEquals("account", account.getObject());
+        assertEquals("/account", account.getLocation());
+    }
+
+    @Test
+    public void testGetEmail() throws IOException, OmiseException {
+        Requester requester = getTestRequester();
+        Request<Account> getAccountRequest = new Account.GetRequestBuilder().build();
+        Account account = requester.sendRequest(getAccountRequest);
+
         assertRequested("GET", "/account", 200);
 
         assertEquals("chakrit@omise.co", account.getEmail());
+    }
+
+    @Test
+    public void testGetUserId() throws IOException, OmiseException {
+        Requester requester = getTestRequester();
+        Request<Account> getAccountRequest = new Account.GetRequestBuilder().build();
+        Account account = requester.sendRequest(getAccountRequest);
+
+        assertRequested("GET", "/account", 200);
         assertEquals("acct_4yq6tcsyoged5c0ocxd", account.getId());
-        assertEquals("account", account.getObject());
-        assertEquals("/account", account.getLocation());
+    }
+
+    @Test
+    public void testGetCurrency() throws IOException, OmiseException {
+        Requester requester = getTestRequester();
+        Request<Account> request = new Account.GetRequestBuilder().build();
+        Account account = requester.sendRequest(request);
+
+        assertRequested("GET", "/account", 200);
+
+        assertEquals("thb", account.getCurrency());
     }
 }
