0c8298143df0f3d7f90bd6c43363c22b5465a021
Nami <namnamsalimi@gmail.com>
2019-06-20 14:25:48 +0700

    Bumped Jackson library version from 2.8.8 to 2.9.9 and also fixed some changed APIs in the new version (#83)

--- src/main/java/co/omise/Serializer.java
+++ src/main/java/co/omise/Serializer.java
@@ -59,10 +59,10 @@ public final class Serializer {
         objectMapper = new ObjectMapper()
                 .registerModule(new JodaModule()
                         .addSerializer(DateTime.class, new DateTimeSerializer()
-                                .withFormat(new JacksonJodaDateFormat(dateTimeFormatter))
+                                .withFormat(new JacksonJodaDateFormat(dateTimeFormatter), 0)
                         )
                         .addSerializer(LocalDate.class, new LocalDateSerializer()
-                                .withFormat(new JacksonJodaDateFormat(localDateFormatter))
+                                .withFormat(new JacksonJodaDateFormat(localDateFormatter), 0)
                         )
                 )
 
