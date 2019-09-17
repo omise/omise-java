520f306bf12daf5617613997773620c9125d1b02
Marut K <marut@omise.co>
2019-09-17 17:01:55 +0700

    Add SourceType.Unknown

--- src/main/java/co/omise/models/SourceType.java
+++ src/main/java/co/omise/models/SourceType.java
@@ -1,26 +1,46 @@
 package co.omise.models;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.lang.reflect.Field;
 
 public enum SourceType {
-    @JsonProperty("alipay")Alipay,
-    @JsonProperty("barcode_alipay")BarcodeAlipay,
-    @JsonProperty("bill_payment_tesco_lotus")BillPaymentTescoLotus,
-    @JsonProperty("econtext")Econtext,
-    @JsonProperty("installment_bay")InstallmentBay,
-    @JsonProperty("installment_bbl")InstallmentBbl,
-    @JsonProperty("installment_first_choice")InstallmentFirstChoice,
-    @JsonProperty("installment_kbank")InstallmentKbank,
-    @JsonProperty("installment_ktc")InstallmentKtc,
-    @JsonProperty("internet_banking_bay")InternetBankingBay,
-    @JsonProperty("internet_banking_bbl")InternetBankingBbl,
-    @JsonProperty("internet_banking_ktb")InternetBankingKtb,
-    @JsonProperty("internet_banking_scb")InternetBankingScb,
-    @JsonProperty("paynow")Paynow,
-    @JsonProperty("points")Points,
-    @JsonProperty("truemoney")TrueMoney;
+    @JsonEnumDefaultValue
+    @JsonProperty("unknown")
+    Unknown,
+    @JsonProperty("alipay")
+    Alipay,
+    @JsonProperty("barcode_alipay")
+    BarcodeAlipay,
+    @JsonProperty("bill_payment_tesco_lotus")
+    BillPaymentTescoLotus,
+    @JsonProperty("econtext")
+    Econtext,
+    @JsonProperty("installment_bay")
+    InstallmentBay,
+    @JsonProperty("installment_bbl")
+    InstallmentBbl,
+    @JsonProperty("installment_first_choice")
+    InstallmentFirstChoice,
+    @JsonProperty("installment_kbank")
+    InstallmentKbank,
+    @JsonProperty("installment_ktc")
+    InstallmentKtc,
+    @JsonProperty("internet_banking_bay")
+    InternetBankingBay,
+    @JsonProperty("internet_banking_bbl")
+    InternetBankingBbl,
+    @JsonProperty("internet_banking_ktb")
+    InternetBankingKtb,
+    @JsonProperty("internet_banking_scb")
+    InternetBankingScb,
+    @JsonProperty("paynow")
+    Paynow,
+    @JsonProperty("points")
+    Points,
+    @JsonProperty("truemoney")
+    TrueMoney;
 
     @Override
     public String toString() {
