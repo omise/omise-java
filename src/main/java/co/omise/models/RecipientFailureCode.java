090cade6590a777c5db9e2bfe30a3c3633dd9454
Marut K <marut@omise.co>
2019-09-17 15:22:06 +0700

    Reorder fields

--- src/main/java/co/omise/models/RecipientFailureCode.java
+++ src/main/java/co/omise/models/RecipientFailureCode.java
@@ -3,7 +3,7 @@ package co.omise.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum RecipientFailureCode {
-    @JsonProperty("name_mismatch")NameMismatch,
     @JsonProperty("account_not_found")AccountNotFound,
     @JsonProperty("bank_not_found")BankNotFound,
+    @JsonProperty("name_mismatch")NameMismatch;
 }
