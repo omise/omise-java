4c5c5c688a7b13fbee501023890863aa79686801
Rezigned <rezigned@users.noreply.github.com>
2020-08-04 16:33:26 +0700

    Add GitHub actions (#109)
    * Add Github Actions

--- src/main/java/co/omise/requests/ResponseType.java
+++ src/main/java/co/omise/requests/ResponseType.java
@@ -1,12 +1,13 @@
 package co.omise.requests;
 
+import co.omise.Client;
 import co.omise.models.OmiseObjectBase;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 /**
  * This class is wrapped data type either Class or TypeReference.
  *
- * @param <T>
+ * @param <T> the generic type for any Model that would need to be returned by the {@link Client} when this request is passed to it
  */
 public class ResponseType<T extends OmiseObjectBase> {
 
